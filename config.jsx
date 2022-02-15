5fbf5facf57ffdfbacf437b8776163d0be27a20e
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-02-15 19:17:51 +0530

    🚀 Finished Market Place

---
+++ config.jsx
@@ -0,0 +1,4 @@
+
+  export const nftmarketaddress = "0x6C745c3Fd7c218613D8acfDf3734a230dB96cE24"
+  export const nftaddress = "0x962B0844f291D06c8E56FCFcC8fc34F0B2c34830"
+  
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,38 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+    <nav className="border-b p-6">
+      <p className="text-4xl font-bold">Metaverse Marketplace</p>
+      <div className="flex mt-4">
+        <Link href="/">
+          <a className="mr-4 text-[#B75CFF]">
+            Home
+          </a>
+        </Link>
+        <Link href="/create-item">
+          <a className="mr-6 text-[#B75CFF]">
+            Sell Digital Asset
+          </a>
+        </Link>
+        <Link href="/my-assets">
+          <a className="mr-6 text-[#B75CFF]">
+            My Digital Assets
+          </a>
+        </Link>
+        <Link href="/creator-dashboard">
+          <a className="mr-6 text-[#B75CFF]">
+            Creator Dashboard
+          </a>
+        </Link>
+      </div>
+    </nav>
+    <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
